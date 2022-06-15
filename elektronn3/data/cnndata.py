d7de9a460503f2b367487160ca5a8789e0b98c83
My-Tien Nguyen <my-tien.nguyen@ariadne.ai>
2022-06-15 10:00:15 +0200

    cnndata Segmentation2d: fix offset cutout of target with more dimensions

--- elektronn3/data/cnndata.py
+++ elektronn3/data/cnndata.py
@@ -665,7 +665,7 @@ class Segmentation2d(data.Dataset):
                 pass
         if np.any(self.offset):
             off = self.offset
-            target = target[off[0]:-off[0], off[1]:-off[1]]
+            target = target[..., off[0]:-off[0], off[1]:-off[1]]
         sample = {
             'inp': torch.as_tensor(inp.astype(self.inp_dtype)),
             'target': torch.as_tensor(target.astype(self.target_dtype)),
